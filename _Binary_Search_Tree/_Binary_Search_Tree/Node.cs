7ed24d7b224564af7da5cc29b3b1d476493f5d14
MR-Design <two7city@gmail.comMR-Design>
2018-10-12 07:20:53 -0500

    Add Methode Done

---
+++ _Binary_Search_Tree/_Binary_Search_Tree/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Binary_Search_Tree
+{
+    class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+
+// Constractor...
+        public Node(int Input)
+        {
+            data = Input;   // Constractor value when it's created
+            left = null;    // New Value Will be Null when we create...
+            right = null;   // ...the node, The Are Not Connected to anything.//
+
+        }
+    }
+}
---
+++ _Binary_Search_Tree/_Binary_Search_Tree/Tree.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Binary_Search_Tree
+{
+    class Tree
+    {
+        Node root; // a Root of Type  Node
+
+   //Constractor
+        public  Tree() // Creating an Empty Tree
+        {
+            root = null;
+
+        }
+
+        public Tree(int Input) // Creating a Tree with an Initial Input 'Root'
+        {
+            root = new Node(Input);
+
+        }
+
+        public void add(int data) 
+        {
+
+            if (root == null) // tree Empty 
+            {
+                //  Need new Node
+                Node newNode = new Node(data);
+                root = newNode;
+                return;
+            }
+            // If Input  Is Less or Equall than Current Node we Move Left 
+            Node currentNode = root;
+            bool added = false;
+            do
+            {
+                if(data <= currentNode.data)//Go Left
+                {
+                    if (currentNode.left == null)
+                    {
+                        Node newNode = new Node(data);
+                        currentNode.left = newNode;
+                        added =true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.left;
+                    }
+                }
+
+                // If Input  Is Greather  than Current Node we Move Right 
+
+                if (data > currentNode.data) //Go Right
+                {
+                    if (currentNode.right == null)
+                    {
+                        Node newNode = new Node(data);
+                        currentNode.right = newNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.right;
+                    }
+                }
+
+            }
+            while (!added);
+        }
+        public void search() // Search Methode
+        {
+
+        }
+    }   
+}
